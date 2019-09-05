21fcc6e008bdeae560c3c322bd890afee53bf962
Adam Carter <mail@adam-carter.com>
2019-09-05 12:59:38 -0700

    adding today's starter code

---
+++ lectures/2019-09-05 CH6 writing a program/Lecture/Lecture/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <exception> //exposes Exception objects
+#include <vector> //CH4 -- replaces basic C-style arrays
+#include "HelperFunctions.h"
+using namespace std;
+
+int main(void)
+{
+	
+
+	return 0;
+}
