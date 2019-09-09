48dacf31625de4ea879ac93dfd2bb78796cf1189
Blake Callison <bbc4@humboldt.edu>
2019-09-09 14:35:28 -0700

    First update of code

--- projects/pa2/pa2/main.cpp
+++ projects/pa2/pa2/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
 using namespace std;
 
 int main(void)
 {
-	cout << "Hello world!";
+	ifstream inPix;
+	inPix.open("C:\Users\bbc4\Desktop\bin\2019-fall-cs112\projects\pa2\pa2\tinypix.ppm");
+
+	// This checks to see if tinypix.ppm was opened
+	if (!inPix)
+	{
+		cerr << "Unable to open file tinypix.ppm";
+		exit(1); //call system to stop
+	}
+
+
 	return 0;
 }
