e7d90b0b8ee3cac5091b23c0a95be0d322b4a051
Candace <cdm103@humboldt.edu>
2019-09-10 12:56:18 -0700

    Update to pa2 main.cpp file 2019/09/10

--- projects/pa2/pa2/main.cpp
+++ projects/pa2/pa2/main.cpp
@@ -1,28 +1,38 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 using namespace std;
 
 
 void image_file(string file_name)
 {
-    string line_to_read
+	string line_to_read;
     
     ifstream file_to_read;
     
 	file_to_read.open(file_name);
-
-	while (!file_to_read.eof())
+	getline(file_to_read, line_to_read);
+	cout << "Image Format: " << line_to_read << endl;
+	getline(file_to_read, line_to_read, ' ');
+	cout << "Width: " << line_to_read << endl;
+	getline(file_to_read, line_to_read);
+	cout << "Height: " << line_to_read << endl;
+	getline(file_to_read, line_to_read);
+	cout << "Max Pixel Value: " << line_to_read << endl;
+	cout << "Data: " << endl;
+	while(!file_to_read.eof())
 	{
-		getline(file_to_read, line_to_read)
-
-		cout <<  << endl;
+		getline(file_to_read, line_to_read);
+		cout << line_to_read << endl;
 	}
+		 
 	file_to_read.close();
 }
 
 int main(void)
 {
-	cout << "hello, world!";
-	return 0
+	image_file("tinypix.ppm");
+
+	return 0;
 }
