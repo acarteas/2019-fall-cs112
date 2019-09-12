dcff61b8d8607811c381b4cdd9c9ee5f7cf9f747
kvh8 <54556865+kvh8@users.noreply.github.com>
2019-09-12 13:50:46 -0700

    PA2

--- projects/pa2/pa2/main.cpp
+++ projects/pa2/pa2/main.cpp
@@ -15,16 +15,39 @@ int main(void)
 {
 	ifstream myfile;
 	string line1; 
-
+	string line2;
+	string line3;
+	string line4;
+	string line5;
+	string line6;
 
 	myfile.open("tinypix.ppm");
 	
-	while (getline(myfile, line1))
+	//use getline outside the while loop for the first three line
+	//Then while loop for the rest
+
+	getline(myfile, line1);
+	cout << "Image Format: " << line1 << endl;
+
+	getline(myfile, line2, ' ');
+	cout << "Width: " << line2 << endl;
+
+	getline(myfile, line3);
+	cout << "Height: " << line3 << endl;
+
+	getline(myfile, line4, ' ');
+	cout << "Max Pixel Value: " << line4 << endl;
+
+	getline(myfile, line5, ' ');
+	cout << "Data: " << line5 << endl;
+
+	while (getline(myfile, line6))
 	{
-		cout << line1 << endl;
+		cout << line6 << endl;
 	}
 
 	myfile.close();
+	
 	
 
 
