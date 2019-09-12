5ef2bfe60d7c76cae95ae495313b72f33f117594
tashenea <tmb15@humboldt.edu>
2019-09-12 13:38:12 -0700

    My PA

--- projects/pa2/pa2/main.cpp
+++ projects/pa2/pa2/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <fstream>
+  
 using namespace std;
+
 
 int main(void)
 {
-	cout << "hello, world!";
-	return 0;
+	int sum = 0;
+	int x;
+	ifstream  myFile;
+
+	myFile.open("tinypix.ppm");
+
+	if (!myFile)
+	{
+		cout << "Not open" << endl;
+		return 0;
+	}
+	 
+	while (myFile >> x)
+	{
+		sum = sum + x;
+
+	}
+	cout << "The file was open";
+	myFile.close();
 }
