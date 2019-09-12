2b9c7ee395182ddaff3035373ff33f0d121eddfb
kvh8 <54556865+kvh8@users.noreply.github.com>
2019-09-12 13:09:32 -0700

    pa2

--- projects/pa2/pa2/main.cpp
+++ projects/pa2/pa2/main.cpp
@@ -14,13 +14,20 @@ using namespace std;
 int main(void)
 {
 	ifstream myfile;
-	string line; 
+	string line1; 
+	string line2;
+	string line3;
+	string line4;
+	string line5;
+	string line6;
+	string line7;
+
 
 	myfile.open("tinypix.ppm");
 	
-	while ( getline(myfile, line));
+	while (getline(myfile, line1))
 	{
-		cout << line << "\n";
+		cout << line1 << endl;
 	}
 
 	myfile.close();
