92e53e73102d7ca25b9218037771569f8229b8f5
iluv0rangejuice <54556887+iluv0rangejuice@users.noreply.github.com>
2019-09-12 13:08:04 -0700

    PA2
    PA 2 code

---
+++ projects/pa2/tinypix.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    ofstream myfile;
+    myfile.open ("example.txt");
+    myfile << "i am typing in the file" << endl;
+    myfile << (10+11) << endl;
+    myfile.close();
+    */
+    
+    string name
+    cout << "Enter name" << endl;
+    getline (cin, name);
+    cout << "Hello, " << name << endl;
+    
+    return 0;
+}
