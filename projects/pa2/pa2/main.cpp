8bd200e4f9966b47836010b7a3a8844e8c6800b6
Candace <cdm103@humboldt.edu>
2019-09-14 21:52:15 -0700

    Added GIF to pa2 assignment

--- projects/pa2/pa2/main.cpp
+++ projects/pa2/pa2/main.cpp
@@ -5,8 +5,9 @@
 using namespace std;
 /*
 signature: string -> void
-purpose: expects a string which represent a file name and returns nothing, but has
-the side effect of printing to the screen with respective formatting. 
+purpose: expects a string which represent a file name and returns 
+nothing,but has the side effect of printing to the screen with 
+respective formatting. 
 */
 
 void image_file(string file_name)
@@ -16,18 +17,24 @@ void image_file(string file_name)
     ifstream file_to_read;
     
 	file_to_read.open(file_name);
+	
 	getline(file_to_read, line_to_read);
 	cout << "Image Format: " << line_to_read << endl;
+	
 	getline(file_to_read, line_to_read, ' ');
 	cout << "Width: " << line_to_read << endl;
+	
 	getline(file_to_read, line_to_read);
 	cout << "Height: " << line_to_read << endl;
+	
 	getline(file_to_read, line_to_read);
 	cout << "Max Pixel Value: " << line_to_read << endl;
 	cout << "Data: " << endl;
+	
 	while(!file_to_read.eof())
 	{
 		getline(file_to_read, line_to_read);
+
 		cout << line_to_read << endl;
 	}
 		 
