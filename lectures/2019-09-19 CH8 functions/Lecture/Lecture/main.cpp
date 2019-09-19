2f332081465b7004476592f7b56aaad618d6dc22
acarter <mail@adam-carter.com>
2019-09-19 10:53:35 -0700

    added today's starter lecture code

---
+++ lectures/2019-09-19 CH8 functions/Lecture/Lecture/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <exception> //exposes Exception objects
+#include <vector> //CH4 -- replaces basic C-style arrays
+#include "HelperFunctions.h"
+
+using namespace std;
+
+
+/*
+Tasks:
+1. Talk about next milestone
+2. Implement functions related to last milestone
+*/
+int main(int argc, char* argv[])
+{
+	
+	return 0;
+}
