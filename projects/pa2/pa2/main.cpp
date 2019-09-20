5e9b4b93cc7b43b49ac5d824ce2d4b840bd2c85d
Blake Callison <bbc@humboldt.edu>
2019-09-19 22:40:23 -0700

    Pa2 Completed

--- projects/pa2/pa2/main.cpp
+++ projects/pa2/pa2/main.cpp
@@ -7,13 +7,45 @@ using namespace std;
 int main(void)
 {
 	ifstream inPix;
-	inPix.open("C:\Users\bbc4\Desktop\bin\2019-fall-cs112\projects\pa2\pa2\tinypix.ppm");
+	inPix.open("tinypix.ppm");
+	string line;
+	int counter = 0;
+	string img_spec;
+	int width, height = 0;
+	int max_color = 0;
 
 	// This checks to see if tinypix.ppm was opened
-	if (!inPix)
+	if (!inPix.is_open())
 	{
 		cerr << "Unable to open file tinypix.ppm";
 		exit(1); //call system to stop
+	}
+
+	while (getline(inPix, line))
+	{
+		counter++;
+		if (counter == 1)
+		{
+			img_spec = line;
+			cout << "Image Format: " << img_spec << endl;
+		}
+		if (counter == 2)
+		{
+			width = stoi(line.substr(0, line.find(" ")));
+			height = stoi(line.substr(line.find(" ")+1,line.length()));
+			cout << "Width: " << width << endl;
+			cout << "Height: " << height << endl;
+		}
+		if (counter == 3)
+		{
+			max_color = stoi(line);
+			cout << "Max Pixel Value: " << max_color << endl;
+			cout << "Data: " << endl;
+		}
+		if (counter > 3)
+		{
+			cout << line << endl;
+		}
 	}
 
 
