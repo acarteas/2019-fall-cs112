933ba1a5faa59ad61e1331bf1c96be59aa035dc6
Tashenea <tmb15@humboldt.edu>
2019-09-23 14:42:41 -0700

    9/23 - updating code with P3 information

--- projects/pa2/Project 2/Project 2/main.cpp
+++ projects/pa2/Project 2/Project 2/main.cpp
@@ -7,8 +7,7 @@ using namespace std;
 
 int main(void)
 {
-	int sum = 0;
-	int x;
+	
 	ifstream  myFile;
 
 	myFile.open("tinypix.ppm");
@@ -25,6 +24,37 @@ int main(void)
 		getline(myFile, line);
 		cout << line << endl;
 	}
-	
+
 	myFile.close();
+	//string x;
+	//cout << "Enter source File:" << endl;
+	//cin >> x;
+	//cout << "Enter source File " << x << endl;
+
+	//string y;
+	//cout << "Enter destination file:" << endl;
+	//cin >> y;
+
+	//cout << "Progam Complete." << endl;
+
+	/*
+	myFile -> int (I want to make a copy of the orginal file )
+	
+
+	Ask the the user to type in the file of their choice -> "tinypix.ppm"
+	print out Enetr the source file and the persons input 
+			if the user inputs a file that is not in the directory 
+			print out a message saying file not included please try again.
+			else 
+			print out file was found
+
+
+	Ask the user where they would like to put this file ( destination) 
+			if the user inputs a file that is not in the directory 
+			print out a message saying file not included please try again.
+			else 
+			print out file was found 
+
+	I want to sort through the file's content 
+	*/
 }
