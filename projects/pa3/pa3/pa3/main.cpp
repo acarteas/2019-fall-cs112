fbc22282430e4a86f6b41679ce175538a1d2e3a4
zjp29 <zjp29@RDSH-VLAB-01>
2019-09-30 09:46:39 -0700

    finished assignment

--- projects/pa3/pa3/pa3/main.cpp
+++ projects/pa3/pa3/pa3/main.cpp
@@ -24,71 +24,68 @@ vector<string> getFile(void)
 	cin >> file_name;
 	cout << "You entered: " << file_name << endl;
 
-	/*cout << "Enter the name of the output file: ";
+
+	// opens the file that the user provided -->
+	ifstream file(file_name);
+	vector<string> data{}; // vector for file content
+
+	if (file.is_open() == true) // if the file is open..
+	{
+		string myline;
+		while (file >> myline)
+		{
+			data.push_back(myline); // push all elements to individual items in vector of strings
+		}
+		cout << "file stored to vector." << endl;
+		// grab header info
+		string width;
+		string height;
+		string format;
+		string M_value;
+		width = data[1];
+		height = data[2];
+		format = data[0];
+		M_value = data[3];
+		// print header info
+		cout << "Format: " << format << endl
+			 << "Width: " << width << endl
+			 << "Height: " << height << endl
+			 << "Max Color Value: " << M_value << endl;
+	}
+	file.close(); // close the file now that its contents are stored to a vector
+	return data;
+}
+
+int zeroEveryThird(vector<string> data)
+{
+	string new_file;
+	string path;
+	cout << "Enter the name of the output file: ";
 	cin >> new_file;
 	cout << "You entered: " << new_file << endl;
 
 	cout << "Enter an absolute path where you want the new file to be stored: ";
 	cin >> path;
-	cout << "You entered: " << path << endl;*/
+	cout << "You entered: " << path << endl;
+	path = path + new_file;// concatenate path and file name for ofstream constructor
+	cout << path << endl;
 
+	ofstream output_img(path); // initialize output stream
 
-	// opens the file that the user provided -->
-	ifstream file(file_name);
-	vector<string> ender{}; // vector for file content
-
-	if (file.is_open() == true) // if the file is open..
-	{
-		string myline;
-		
-
-		while (file >> myline)
-		{
-			ender.push_back(myline);
-			cout << myline << endl;
-		}
-		
-
-		
-		// grab width and height
-		//istringstream row2{ ender[1] };
-		//string temp;
-
-		// output all header elements before modification
-		/*cout << "Image Format: " << ender[0] << endl;
-		getline(row2, temp, ' ');
-		int width = stoi(temp);
-		getline(row2, temp, ' ');
-		int height = stoi(temp);
-		cout << "Width: " << width << endl
-			<< "Height: " << height << endl
-			<< "Max Color Value: " << ender[2] << endl;
-			*/
-	}
-	file.close(); // close the file now that its contents are stored to a vector
-	return ender;
-}
-
-int zeroEveryThird(vector<string> data)
-{
-	data.erase(data.begin(), data.begin()+3);
-	int test;
-	test = data.size();
-	cout << test << endl;
-	string data_strings;
 	for (int i = 4; i < data.size(); i+=3)
 	{
-		istringstream row{ data[i] };
-		getline(row, data_strings, ' ');
-		cout << data_strings << endl;
+		data[i] = '0'; // change every third
     }
+
+	for (int i = 0; i < data.size(); i += 1) 
+	{
+		output_img << data[i] << endl; // output entire vector to new file
+	}
+	output_img.close();
 	return 0;
 }
 
-
-
 int main()
 {
-	//zeroEveryThird(getFile());
-	getFile();
+	zeroEveryThird(getFile());
 }
