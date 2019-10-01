0891b39c7ae1520bce985bacf7d2f93f12faefa7
Adam Carter <mail@adam-carter.com>
2019-10-01 14:17:29 -0700

    added today's lecture code.

---
+++ lectures/2019-10-01 file IO/Lecture/Lecture/Pixel.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef PIXEL_H
+#define PIXEL_H
+
+#include <istream>
+#include <exception>
+using namespace std;
+
+class Pixel
+{
+public:
+	
+	//if you use this, you are not allowed to keep this as is!
+	//instead, I expect you to write proper getters and setters and
+	//make the actual variables private!
+	int red = 0;
+	int green = 0;
+	int blue = 0;
+};
+
+istream& operator>>(istream& stream, Pixel& pixel)
+{
+	stream >> pixel.red >> pixel.green >> pixel.blue;
+	return stream;
+}
+
+ostream& operator<<(ostream& stream, Pixel& pixel)
+{
+	stream << pixel.red << " " << pixel.green << " " << pixel.blue;
+	return stream;
+}
+
+#endif // !PIXEL_H
