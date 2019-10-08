3cef76fc7375c08631bd90930519a60277264feb
Adam Carter <mail@adam-carter.com>
2019-10-08 11:00:43 -0700

    added today's starter lecture code.

---
+++ lectures/2019-10-08 CH11 custom IO/Lecture/Lecture/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <cmath>
+#include <ctime>
+#include <exception> //exposes Exception objects
+#include <vector> //CH4 -- replaces basic C-style arrays
+#include <iomanip> //for making fancy couts and output to file
+#include "HelperFunctions.h"
+#include "FileHelpers.h"
+
+//P. 388 Try this: Format data (Adam's spin: format CSV data!)
+void formatData()
+{
+
+}
+
+//P. 398 Removing punctuation from words
+vector<string> removePunctuation(vector<string>& raw_text)
+{
+	vector<string> result{};
+	return result
+}
+
+int main(int argc, char* argv[])
+{
+	
+	return 0;
+}
